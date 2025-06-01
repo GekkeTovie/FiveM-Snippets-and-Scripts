90c2be491fee2aef4fef8bedb2208b8d77bb28c0
GekkeTovie <143883097+GekkeTovie@users.noreply.github.com>
2025-06-01 21:09:12 +0200

    Delete removeobject/sv_removeprops.lua

--- removeobject/sv_removeprops.lua
+++
@@ -1,5 +0,0 @@
-RegisterServerEvent('removeProps:requestDelete')
-AddEventHandler('removeProps:requestDelete', function()
-    local src = source
-    TriggerClientEvent('removeProps:deleteProps', src)
-end)
