b32b0f42c26738011301af4c83649ca471cea9c8
GekkeTovie <143883097+GekkeTovie@users.noreply.github.com>
2025-06-01 21:08:47 +0200

    Create fxmanifest.lua

---
+++ removeobject/fxmanifest.lua
@@ -0,0 +1,15 @@
+-- Resource Metadata
+fx_version 'bodacious'
+games { 'gta5' }
+
+author 'GekkeTovie' --original author
+description 'Removes props that have been entered in the clientside by ID'
+version '1.0.0'
+
+-- shared_scripts {
+--     'config.lua'
+-- }
+
+client_script {
+    'client/cl_removeprops.lua',
+}
